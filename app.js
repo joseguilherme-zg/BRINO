ec7c6135ced5869d56b2e0b40835e22505a61fc2
joseguilherme-zg <zansavio.goncalves@escola.pr.gov.br>
2025-09-30 09:15:06 -0300

    update

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const container = document.getElementById('fotos')
-const imagens = container.getElementsByTagName('img');
-let verify = false
+const imagens = container.querySelectorAll('img');
+let foto_troca = false
 
-container.addEventListener('click', ()=> {
-        for (let i = 0; i < imagens.length; i++) {
+function trocaFoto(){
+        for (let i = 0; i < imagens.length; i++){
         if(foto_troca === false){
         imagens[i].src = `baixados(${i + 9}).jpg`;
         foto_troca = true;
-    }
-
     }else{
         for (let i = 0; i < imagens.length; i++) {
             imagens[i].src = `baixados(${i+1}).jpg`;
             foto_troca = false;
         }}
-    });
+    }};
